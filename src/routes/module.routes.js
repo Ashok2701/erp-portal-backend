3c4a16f0114d77df8a406a74fded7217d4f74f1f
Ashok2701 <61420638+Ashok2701@users.noreply.github.com>
2026-02-14 23:15:51 +0530

    admin_correction4

--- src/routes/module.routes.js
+++ src/routes/module.routes.js
@@ -4,10 +4,10 @@ const ModuleController = require("../controllers/module.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 const adminMiddleware = require("../middleware/admin.middleware");
 
-router.post("/", authMiddleware, adminMiddleware, ModuleController.createModule);
-router.get("/", authMiddleware, adminMiddleware, ModuleController.getAllModules);
-router.put("/:id", authMiddleware, adminMiddleware, ModuleController.updateModule);
-router.delete("/:id", authMiddleware, adminMiddleware, ModuleController.deleteModule);
+router.post("/", authMiddleware, ModuleController.createModule);
+router.get("/", authMiddleware, ModuleController.getAllModules);
+router.put("/:id", authMiddleware, ModuleController.updateModule);
+router.delete("/:id", authMiddleware, ModuleController.deleteModule);
 
 // For sidebar (any authenticated user)
 router.get("/active/list", authMiddleware, ModuleController.getActiveModules);
