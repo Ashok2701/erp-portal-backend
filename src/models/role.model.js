a84b702e0c2b53f12fc6108045576b094165b7ab
Ashok2701 <61420638+Ashok2701@users.noreply.github.com>
2026-02-14 17:34:48 +0530

    roles_correction

--- src/models/role.model.js
+++ src/models/role.model.js
@@ -5,7 +5,7 @@ exports.getRolesByUserId = async (userId) => {
 
     const result = await pool.query(
    `
-    SELECT 
+    SELECT r.role_name
     FROM user_roles ur
     JOIN roles r ON ur.role_id = r.role_id
     WHERE ur.user_id = $1
