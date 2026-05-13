36970d404112ba17713d6a9c1ed0cfee2df838aa
Ashok2701 <61420638+Ashok2701@users.noreply.github.com>
2026-05-13 15:37:12 +0530

    soap 2 update
    soap 2 update

--- src/utils/soapClient.js
+++ src/utils/soapClient.js
@@ -1,61 +1,45 @@
 const soap = require("soap");
 const https = require("https");
-const axios = require("axios");
 
 let cachedClient = null;
 
-// IMPORTANT
-// Use WSDL URL here
+// DEV ONLY
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const wsdlUrl =
   process.env.X3_WSDL_URL ||
   "https://tmsx3em.tema-systems.com/soap-wsdl/syracuse/collaboration/syracuse/CAdxWebServiceXmlCC?wsdl";
 
-// Disable SSL verification (DEV ONLY)
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 async function getSoapClient() {
+
   try {
+
     if (cachedClient) {
       return cachedClient;
     }
 
-    // HTTPS Agent
     const httpsAgent = new https.Agent({
       rejectUnauthorized: false,
     });
 
-    // Create SOAP Client
-    const client = await soap.createClientAsync(wsdlUrl, {
-      endpoint: process.env.X3_SOAP_URL,
+    const client = await soap.createClientAsync(
+      wsdlUrl,
+      {
+        endpoint: process.env.X3_SOAP_URL,
 
-      attributesKey: "attributes",
-      valueKey: "$value",
-      xmlKey: "$xml",
+        wsdl_options: {
+          agent: httpsAgent,
+          rejectUnauthorized: false,
+          strictSSL: false,
+        },
 
-      wsdl_options: {
-        httpsAgent,
-        rejectUnauthorized: false,
-        strictSSL: false,
-      },
+        attributesKey: "attributes",
+        valueKey: "$value",
+        xmlKey: "$xml",
+      }
+    );
 
-      request: (options, callback) => {
-        options.httpsAgent = httpsAgent;
-        options.rejectUnauthorized = false;
-
-        axios({
-          ...options,
-          httpsAgent,
-        })
-          .then((response) => {
-            callback(null, response, response.data);
-          })
-          .catch((error) => {
-            callback(error);
-          });
-      },
-    });
-
-    // Basic Authentication
+    // BASIC AUTH
     client.setSecurity(
       new soap.BasicAuthSecurity(
         process.env.X3_USERNAME,
@@ -63,24 +47,28 @@ async function getSoapClient() {
       )
     );
 
-    // DEBUG LOGGING
-    client.on("request", (xml) => {
-      console.log("========== SOAP REQUEST ==========");
-      console.log(xml);
-    });
+    // FORCE AGENT
+    client.httpClient._request = function (url, data, callback, exheaders, exoptions) {
 
-    client.on("response", (xml) => {
-      console.log("========== SOAP RESPONSE ==========");
-      console.log(xml);
-    });
+      const options = {
+        ...exoptions,
+        httpsAgent,
+        rejectUnauthorized: false,
+      };
+
+      return this.request(url, data, callback, exheaders, options);
+    };
+
+    console.log("SOAP CLIENT INITIALIZED");
 
     cachedClient = client;
 
-    console.log("SOAP Client Initialized");
-
     return client;
+
   } catch (error) {
+
     console.error("SOAP CLIENT ERROR:", error);
+
     throw error;
   }
 }
